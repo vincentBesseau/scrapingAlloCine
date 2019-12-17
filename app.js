6153b0982a8f409ac2ce61287687900a2df76668
vincentBesseau <vbesseau44@gmail.com>
2019-12-17 22:49:51 +0100

    Change script to module

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
 const scraping = require('./scraping');
 const loadUrl = require('./loadUrl');
-const args = require('minimist')(process.argv.slice(2))
-// default : UGC Nantes
-let cinema = 'P0666';
-let day = new Date();
-let filtre = {
-    specificMovie: null,
-    withoutTime: false,
-    languageType: null
-};
 
-if (args) {
-    Object.keys(args).forEach((key) => {
-        switch (key) {
-            case 'd':
-            case 'day':
-                day = new Date(args[key]);
-                break;
-            case 'c':
-            case 'cinema':
-                cinema = args[key];
-                break;
-            case 'm':
-            case 'movie':
-                filtre.specificMovie = args[key];
-                break;
-            case 'w':
-            case 'without-time':
-                filtre.withoutTime = true;
-                break;
-            case 'l':
-            case 'language':
-                filtre.languageType = args[key];
-                break;
+const getInfo = async (arguments) => {
+    const arg = await checkArg(arguments)
+    if (arg.cinema !== null) {
+        if (typeof arg.siteUrl !== 'undefined') {
+            const $ = await loadUrl.fetchData(arg.siteUrl);
+            const movies = await scraping.scrapingData($, arg.filtre);
+            return JSON.stringify(movies);
+        } else {
+            throw new Error('Url not found');
         }
-    })
+    } else {
+        throw new Error('Parameter "cinema" or "c" is mandatory to know the targeted cinema');
+    }
 }
 
-const siteUrl = `http://www.allocine.fr/seance/salle_gen_csalle=${cinema}.html`;
+const checkArg = async (arguments) => {
 
-const start = async () => {
-    if (typeof siteUrl !== 'undefined') {
-        const $ = await loadUrl.fetchData(siteUrl);
-        const movies = await scraping.scrapingData($, filtre);
-        return JSON.stringify(movies);
-    } else {
-        throw new URIError('Url not found');
+    // Default value
+    // ex : UGC Nantes P0666
+    let cinema = '';
+    let day = new Date();
+    let filtre = {
+        specificMovie: null,
+        withoutTime: false,
+        languageType: null
+    };
+
+    if (arguments) {
+        Object.keys(arguments).forEach((key) => {
+            switch (key) {
+                case 'd':
+                case 'day':
+                    day = new Date(arguments[key]);
+                    break;
+                case 'c':
+                case 'cinema':
+                    cinema = arguments[key];
+                    break;
+                case 'm':
+                case 'movie':
+                    filtre.specificMovie = arguments[key];
+                    break;
+                case 'w':
+                case 'without-time':
+                    filtre.withoutTime = true;
+                    break;
+                case 'l':
+                case 'language':
+                    filtre.languageType = arguments[key];
+                    break;
+            }
+        })
     }
 
+    return {
+        cinema: cinema,
+        siteUrl: `http://www.allocine.fr/seance/salle_gen_csalle=${cinema}.html`,
+        day: day,
+        filtre: filtre
+    }
 }
 
-return start()
-    .then((movies) => {
-        return movies
-    })
-    .catch((e) => {
-        console.log('error rises : ', e.message);
-    });
+module.exports = {
+    getInfo: getInfo
+}
